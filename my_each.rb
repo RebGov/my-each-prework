59085d343e9a688ecbe65e897b50169be2b95a60
Rebecca Govert <becci.govert@gmail.com>
2018-06-14 14:35:47 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,26 +1,26 @@
 def my_each(array)
  if block_given?
  i = 0 
-  
     while i < array.length 
        yield (array[i])
         i += 1
     end
-    array
+      array
   else
      "No block given"
-  end
+ end
 end
 
 collection = [1, 2, 3, 4]
 my_each(collection) do |i|
-   i 
+  i * 2
 end
+
 #output is [1, 2, 3, 4]
 
 =begin
 my_each(collection) do |i|
- puts i
+  put (+s) i
 end
 
 =end
